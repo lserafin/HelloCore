efd8c1d4b1d22b102f2ba245a1fcf35594f8c26d
Luzian Serafin <luzianserafin@gmail.com>
2016-07-02 16:41:08 +0200

    Updated hello text ;)

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApplication
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello .NET Core 1.0!");
         }
     }
 }
